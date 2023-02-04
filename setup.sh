d9896d583177f3d8a4eb4ae2d0e054efd0ec2cc2
Simon Kuwerin <kuwerin@gmail.com>
2023-02-04 11:11:46 +0300

    Update `setup.sh`

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-ln -s $PWD/.config/ ~/.config/
-
+ln -s $PWD/.config/lsd/ ~/config
+ln -s $PWD/.config/bottom/ ~/config
+ln -s $PWD/.config/nvim/ ~/config
+ln -s $PWD/.zshrc ~/.zshrc
+ln -s $PWD/.mongorc.js ~/.mongorc.js
